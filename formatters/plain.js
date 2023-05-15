846d68f7f2ad220d4f6cfc836a19c62af9e17d2c
ibanb <ibonburger@mail.ru>
2023-05-15 19:42:59 +0300

    fix some

--- formatters/plain.js
+++ formatters/plain.js
@@ -49,10 +49,10 @@ function plain(diff) {
     
             if (type === 'complex') {
                 if (checkPair) {
-                  result.push(`Property '${stackPaths.join('.')}' was updated. From '${type === 'complex' ? '[complex value]' : value}' to '${pairType === 'complex' ? '[complex value]' : getValue(pair)}'`);
+                  result.push(`Property '${stackPaths.join('.')}' was updated. From ${type === 'complex' ? '[complex value]' : value} to '${pairType === 'complex' ? '[complex value]' : getValue(pair)}'`);
                 } else {
                   if (sign === '+') {
-                    result.push(`Property '${stackPaths.join('.')}' was added with value: '[complex value]'`);
+                    result.push(`Property '${stackPaths.join('.')}' was added with value: [complex value]`);
                   }
                   if (sign === '-') {
                     result.push(`Property '${stackPaths.join('.')}' was removed`);
