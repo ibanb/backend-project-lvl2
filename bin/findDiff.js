5e1a8419c45f481925f19657ea9b8d8a773a52d1
ibanb <ibonburger@mail.ru>
2023-05-16 19:15:18 +0300

    flat compare

--- bin/findDiff.js
+++ bin/findDiff.js
@@ -1,17 +1,55 @@
-import { treeCreate } from "./tree.js";
 // import all methods for work with treeNode 
 
 // THIS MAIN func that create diff recursive in obj
 // attention SEE in STD task)))
 
+  // Короче всё ясно. 
+  // 1) найти ключи которые есть в первом но их нет во втором и добавить их с правильными знаками
+  // 2) найти ключи которые есть во втором но их нет в первом и добавить их с правильными знаками
+  // 3) Найти ключи которые есть в обоих и они простые
+  // 4) Найти ключи которые есть в обоих и они комплексные
+  // 5) Провести сортировку ключей на выходе (использовать LODASH)
+
 export default function findDiff(dataOne, dataTwo) {
+
+  const result = {};
   
-  const firstTree = treeCreate(dataOne);
-  const secondTree = treeCreate(dataTwo);
+  const propsDataOne = Object.entries(dataOne);
+  const propsDataTwo = Object.entries(dataTwo);
+  const uniquePropsDataOne = propsDataOne.filter(prop => {
+    const [key, value] = prop;
+    const keysOfPropsDataTwo = propsDataTwo.map(item => item[0]);
+    return !keysOfPropsDataTwo.includes(key);
+  });
+  const uniquePropsDataTwo = propsDataTwo.filter(prop => {
+    const [key, value] = prop;
+    const keysOfPropsDataOne = propsDataOne.map(item => item[0]);
+    return !keysOfPropsDataOne.includes(key);
+  });
+  const commonProps = propsDataOne.filter(prop => {
+    const [key, value] = prop;
+    const keysOfPropsDataTwo = propsDataTwo.map(item => item[0]);
+    return keysOfPropsDataTwo.includes(key);
+  });
 
-  const compareTrees = (nodeOne, nodeTwo) => {
-    return 'diff';
-  }
+  uniquePropsDataOne.map(prop => {
+    const [key, value] = prop;
+    result[`- ${key}`] = value;
+  });
 
-  return compareTrees(firstTree, secondTree);
+  uniquePropsDataTwo.map(prop => {
+    const [key, value] = prop;
+    result[`+ ${key}`] = value;
+  });
+
+  commonProps.map(prop => {
+    // first case is prime value
+    // 
+    const [key, value] = prop;
+  })
+  
+
+  // lodash method for sort
+  return commonPropsDataOne;
+
 };
--- bin/tree.js
+++
@@ -1,9 +0,0 @@
-
-const treeCreate = (obj) => {
-  // recursive create newTree from obj
-  return {};
-};
-
-// all additional methods for node of tree work
-
-export {treeCreate};
