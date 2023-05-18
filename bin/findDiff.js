dd60a86e3069b75a8bfcd4ed190ec1a558618ff9
ibanb <ibonburger@mail.ru>
2023-05-18 10:35:02 +0300

    done

--- bin/findDiff.js
+++ bin/findDiff.js
@@ -1,11 +1,9 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import sort from './sort.js';
 import normalize from './normalize.js';
 
 export default function findDiff(objOne, objTwo) {
-
   const iter = (dataOne, dataTwo) => {
-
     const propsDataOne = Object.entries(dataOne);
     const propsDataTwo = Object.entries(dataTwo);
 
@@ -24,47 +22,51 @@ export default function findDiff(objOne, objTwo) {
       const keysOfPropsDataTwo = propsDataTwo.map((item) => item[0]);
       return keysOfPropsDataTwo.includes(key);
     }).map((prop) => prop[0]);
-    
-    const commonKP = keysCommonProps.reduce((acc, key) => {
-      const preperedUniquePDO = Object.fromEntries(uniquePropsDataOne.map(([KeY, value]) => [`- ${KeY}`, value]));
-      const preperedUniquePDT = Object.fromEntries(uniquePropsDataTwo.map(([KeY, value]) => [`+ ${KeY}`, value]));
-      const uniteUnique = {...preperedUniquePDO, ...preperedUniquePDT};
-      // handle commonKeys
-      const dataOneValue = dataOne[key];
-      const dataTwoValue = dataTwo[key];
-      // console.log(dataOneValue);
-      const typeDataOneValue = typeof dataOneValue === 'object' && dataOneValue !== null ? 'complex' : 'prime';
-      const typeDataTwoValue = typeof dataTwoValue === 'object' && dataTwoValue !== null ? 'complex' : 'prime';
 
-      // types different
-      if (typeDataOneValue !== typeDataTwoValue) {
-        acc[`- ${key}`] = dataOneValue;
-        acc[`+ ${key}`] = dataTwoValue;
-        return {...acc, ...uniteUnique};
-      }
-      // types prime
-      if (typeDataOneValue === typeDataTwoValue && typeDataOneValue === 'prime') {
-        if (dataOneValue === dataTwoValue) {
-          acc[`  ${key}`] = dataOne[key];
-          return {...acc, ...uniteUnique};
-        }
+    const commonKP = keysCommonProps.reduce(
+      (acc, key) => {
+        const preperedUniquePDO = Object.fromEntries(uniquePropsDataOne.map(([KeY, value]) => [`- ${KeY}`, value]));
+        const preperedUniquePDT = Object.fromEntries(uniquePropsDataTwo.map(([KeY, value]) => [`+ ${KeY}`, value]));
+        const uniteUnique = { ...preperedUniquePDO, ...preperedUniquePDT };
+        // handle commonKeys
+        const dataOneValue = dataOne[key];
+        const dataTwoValue = dataTwo[key];
+        // console.log(dataOneValue);
+        const typeDataOneValue = typeof dataOneValue === 'object' && dataOneValue !== null ? 'complex' : 'prime';
+        const typeDataTwoValue = typeof dataTwoValue === 'object' && dataTwoValue !== null ? 'complex' : 'prime';
 
-        if (dataOneValue !== dataTwoValue) {
+        // types different
+        if (typeDataOneValue !== typeDataTwoValue) {
           acc[`- ${key}`] = dataOneValue;
           acc[`+ ${key}`] = dataTwoValue;
-          return {...acc, ...uniteUnique};
+          // return { ...acc, ...uniteUnique };
         }
-      }
+        // types prime
+        if (typeDataOneValue === typeDataTwoValue && typeDataOneValue === 'prime') {
+          if (dataOneValue === dataTwoValue) {
+            acc[`  ${key}`] = dataOne[key];
+            // return { ...acc, ...uniteUnique };
+          }
 
-      // types complex
-      if (typeDataOneValue === typeDataTwoValue && typeDataOneValue === 'complex') {
-        acc[`  ${key}`] = iter(dataOneValue, dataTwoValue);
-        return {...acc, ...uniteUnique};
-      }}, 
-    {});
-      
-      return commonKP;
-  }
+          if (dataOneValue !== dataTwoValue) {
+            acc[`- ${key}`] = dataOneValue;
+            acc[`+ ${key}`] = dataTwoValue;
+            // return { ...acc, ...uniteUnique };
+          }
+        }
+
+        // types complex
+        if (typeDataOneValue === typeDataTwoValue && typeDataOneValue === 'complex') {
+          acc[`  ${key}`] = iter(dataOneValue, dataTwoValue);
+        }
+
+        return { ...acc, ...uniteUnique };
+      },
+      {},
+    );
+
+    return commonKP;
+  };
 
   const result = iter(objOne, objTwo);
   const sorted = sort(result);
@@ -72,5 +74,3 @@ export default function findDiff(objOne, objTwo) {
 
   return normalized;
 }
-
-
--- bin/normalize.js
+++ bin/normalize.js
@@ -2,7 +2,6 @@ export default function normalize(diff) {
   const signs = ['-', '+', ' '];
 
   const iter = (item) => {
-    
     if (typeof item !== 'object' || item === null) {
       return item;
     }
@@ -12,22 +11,23 @@ export default function normalize(diff) {
       // prime
       if (typeOfValue === 'prime' && signs.includes(key.slice(0, 1))) {
         acc[key] = value;
-        return acc;
+        // return acc;
       }
       if (typeOfValue === 'prime' && !signs.includes(key.slice(0, 1))) {
         acc[`  ${key}`] = value;
-        return acc;
+        // return acc;
       }
       // complex
       if (typeOfValue === 'complex' && signs.includes(key.slice(0, 1))) {
         acc[key] = iter(value);
-        return acc;
+        // return acc;
       }
 
       if (typeOfValue === 'complex' && !signs.includes(key.slice(0, 1))) {
         acc[`  ${key}`] = iter(value);
-        return acc;
+        // return acc;
       }
+      return acc;
     }, {});
 
     return result;
